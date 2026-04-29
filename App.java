67ade097e2dbb567ef9af1404514352986d03df9
Sofia Perez <sofia16godoy@gmail.com>
2026-04-29 17:52:45 +0200

    caca

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.err.println("MIENTRAS SOFIA SEA SOFIA SOFIEMOS");
+        System.out.println("caca");
     }
 }
